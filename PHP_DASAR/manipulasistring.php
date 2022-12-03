61e97e5f09b9ab854da8f87a71d961728def263f
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-12-03 07:27:14 +0700

    belajar manipulasi string

--- PHP_DASAR/manipulasistring.php
+++ PHP_DASAR/manipulasistring.php
@@ -5,6 +5,7 @@ $name = "Adnan Erlansyah";
 echo "Name : " . $name . PHP_EOL;
 echo "Value : " . 100 . PHP_EOL;
 
+// konversi tipe data
 $valuestring = (string)100;
 var_dump($valuestring);
 
@@ -14,4 +15,13 @@ var_dump($valueInteger);
 $valueFloat = (float)"1.10";
 var_dump($valueFloat);
 
+// menangakses string
 $name = "Adnan";
+echo $name[0] . PHP_EOL;
+echo $name[1] . PHP_EOL;
+echo $name[2] . PHP_EOL;
+
+// Variable Parsing & Curly Brace
+$name = "Adnan";
+$age = 10;
+echo "Hello {$name}', Selamat Belajar. Umur mu $age" . PHP_EOL;
