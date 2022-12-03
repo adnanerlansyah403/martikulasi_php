fac27960fafbefdf431c2296d279890d5d349d33
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-12-03 07:21:28 +0700

    Belajar PHP DASAR

---
+++ PHP_DASAR/manipulasistring.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "Adnan Erlansyah";
+
+echo "Name : " . $name . PHP_EOL;
+echo "Value : " . 100 . PHP_EOL;
+
+$valuestring = (string)100;
+var_dump($valuestring);
+
+$valueInteger = (int)"100";
+var_dump($valueInteger);
+
+$valueFloat = (float)"1.10";
+var_dump($valueFloat);
+
+$name = "Adnan";
